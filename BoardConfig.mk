0bc4f8c38993598c78fbcb3966be64eed53fc9bf
Nanhumly <1607616422@qq.com>
2022-07-23 05:35:45 +0000

    chiron: Add TWRP flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,4 +110,22 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.base@1.0.so
+    
+# TWRP specific build flags
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := 2047
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_FB2PNG := true
+TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := hbtp_vm
+TW_MAX_BRIGHTNESS := 4095
+TW_SCREEN_BLANK_ON_BOOT := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TW_THEME := portrait_hdpi
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone5/temp
 
